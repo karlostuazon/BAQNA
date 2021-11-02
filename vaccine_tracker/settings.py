5ab7455302228b61986e1bbf816cb6fa9cb4d6c3
karlostuazon <karlostuazon@gmail.com>
2021-11-02 11:08:13 +0800

    Update settings.py

--- vaccine_tracker/settings.py
+++ vaccine_tracker/settings.py
@@ -127,15 +127,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'vaccine_tracker/static/tracker/images')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'vaccine_tracker/static/tracker/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
