ef8709fa370e409575f80f5758470324ee6bc349
karlostuazon <karlostuazon@gmail.com>
2021-11-02 10:56:26 +0800

    Update settings.py

--- vaccine_tracker/settings.py
+++ vaccine_tracker/settings.py
@@ -128,11 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'vaccine_tracker/static/tracker/images')
 
