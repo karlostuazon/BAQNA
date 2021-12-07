95f1b9759b224b64f9aa37b75f1c97815cea8164
karlostuazon <karlostuazon@gmail.com>
2021-12-07 13:40:47 +0800

    update password for email - may vax and appt na version

--- vaccine_tracker/settings.py
+++ vaccine_tracker/settings.py
@@ -167,4 +167,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'planter121@gmail.com'
-EMAIL_HOST_PASSWORD = 'planterroot'
+#EMAIL_HOST_PASSWORD = 'planterroot'
+EMAIL_HOST_PASSWORD = 'rtypjwdrdxjnnlwv'
